6b5934d48cee26519705cea19cd05b93892916c4
Javier Garcia <emigarcia2007@gmail.com>
2024-12-07 14:06:00 -0600

    Ejercicios de Ejemplo

--- Validación.py
+++ Validación.py
@@ -1,8 +1,8 @@
 User = "Jaero"
 Password = "OneTwoThree"
 
-user1 = input("Ingresa tu usuario")
-password1 = input("Ingresa tu contraseña")
+user1 = input("Ingresa tu usuario: ")
+password1 = input("Ingresa tu contraseña: ")
 if user1 == User and password1 == Password:
     print("Acceso concedido")
 else:
